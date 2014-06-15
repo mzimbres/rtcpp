68ff54d67e67e21de3a78a51262c3e13e6672244
Marcelo <mzimbres@gmail.com>
2014-06-15 11:17:37 -0300

    Fix in flist::remove_if.

--- test_flist.cpp
+++ test_flist.cpp
@@ -97,6 +97,7 @@ class flist {
         *p1 = p2;
         continue;
       }
+      tail = p2;
       p1 = &p2->llink;
       p2 = p2->llink;
     }
@@ -106,14 +107,29 @@ class flist {
 int main()
 {
   flist<int> l(10);
-  for (int i = 0; i < 10; ++i)
+  for (int i = 0; i < 4; ++i)
     if (!l.push_back(i))
       break;
-  l.remove_if(4);
-  l.remove_if(9);
   l.push_back(100);
-  l.push_back(200);
-  l.push_back(300);
+  l.push_back(100);
+  l.push_back(100);
+  l.push_back(100);
+  l.push_back(100);
+  l.push_back(100);
+  l.remove_if(100);
+  //l.remove_if(5);
+  //l.remove_if(6);
+  //l.remove_if(9);
+  //l.remove_if(8);
+  l.push_back(100);
+  l.push_back(100);
+  l.push_back(100);
+  l.push_back(100);
+  l.push_back(100);
+  l.push_back(100);
+  l.remove_if(100);
+  //l.push_back(200);
+  //l.push_back(300);
   std::copy(std::begin(l), std::end(l), std::ostream_iterator<int>(std::cout, ","));
   std::cout << std::endl;
   return 0;
