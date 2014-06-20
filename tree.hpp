fea3f58965c6cdb347757baa4cf22bccbd93db08
Marcelo <mzimbres@gmail.com>
2014-06-19 21:16:36 -0300

    Separating definitions from declarations.

--- tree.hpp
+++ tree.hpp
@@ -22,63 +22,74 @@ class bst {
   std::vector<node_type> pool;
   node_pointer root;
   node_pointer avail;
-  node_pointer add_node(T key)
-  {
-    if (!avail)
-      return 0; // The tree has exhausted its capacity.
-
-    node_pointer q = avail;
-    avail = avail->llink;
-    q->key = key;
-    q->llink = 0;
-    q->rlink = 0;
-    return q;
-  }
+  node_pointer add_node(T key);
+  bst(const bst& rhs); // To be implemented
+  const bst& operator=(const bst& rhs); // To be implemented
   public:
   node_pointer get_root() const {return root;}
-  bst(std::size_t size)
-  : pool(size)
-  , root(0)
-  , avail(0)
-  {
-    // Let us link the avail stack.
-    pool[0].llink = 0;
-    pool[0].rlink = 0;
-    for (std::size_t i = 1; i < pool.size(); ++i) {
-      pool[i].llink = &pool[i - 1];
-      pool[i].rlink = 0;
-    }
-    avail = &pool.back();
-  }
-  node_pointer insert(T key)
-  {
-    if (!root) { // the tree is empty
-      root = add_node(key);
-      return root;
-    }
-    // We have enough space lets insert the element.
-    node_pointer p = root;
-    for (;;) {
-      if (key < p->key) {
-        if (p->llink) {
-          p = p->llink;
-          continue;
-        }
-        p->llink = add_node(key);
-        return p->llink;
-      } else if (key > p->key) {
-        if (p->rlink) {
-          p = p->rlink;
-          continue;
-        }
-        p->rlink = add_node(key);
-        return p->rlink;
-      } else {
-        return p;
-      }
-    }
-  }
+  bst(std::size_t size);
+  node_pointer insert(T key);
   iterator begin() const {return iterator(root);}
   iterator end() const {return iterator();}
 };
+
+template <typename T>
+bst<T>::bst(std::size_t size)
+: pool(size)
+, root(0)
+, avail(0)
+{
+  // Let us link the avail stack.
+  pool[0].llink = 0;
+  pool[0].rlink = 0;
+  for (std::size_t i = 1; i < pool.size(); ++i) {
+    pool[i].llink = &pool[i - 1];
+    pool[i].rlink = 0;
+  }
+  avail = &pool.back();
+}
+
+template <typename T>
+typename bst<T>::node_pointer bst<T>::add_node(T key)
+{
+  if (!avail)
+    return 0; // The tree has exhausted its capacity.
+
+  node_pointer q = avail;
+  avail = avail->llink;
+  q->key = key;
+  q->llink = 0;
+  q->rlink = 0;
+  return q;
+}
+
+template <typename T>
+typename bst<T>::node_pointer bst<T>::insert(T key)
+{
+  if (!root) { // the tree is empty
+    root = add_node(key);
+    return root;
+  }
+  // We have enough space lets insert the element.
+  node_pointer p = root;
+  for (;;) {
+    if (key < p->key) {
+      if (p->llink) {
+        p = p->llink;
+        continue;
+      }
+      p->llink = add_node(key);
+      return p->llink;
+    } else if (key > p->key) {
+      if (p->rlink) {
+        p = p->rlink;
+        continue;
+      }
+      p->rlink = add_node(key);
+      return p->rlink;
+    } else {
+      return p;
+    }
+  }
+}
 
