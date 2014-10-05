53039893dfe46f7f6958920e78144ae6850f0b9d
Marcelo <mzimbres@gmail.com>
2014-10-05 13:41:30 -0300

    Imrovements in make_rand_data function.

--- tests/test_comp_count_sort.cpp
+++ tests/test_comp_count_sort.cpp
@@ -4,17 +4,16 @@
 #include <algorithm>
 #include <functional>
 
+#include <utility/make_rand_data.hpp>
 #include <sort/comp_counting_sort.hpp>
 
 int main()
 {
+  using namespace rtcpp;
   const int size = 50;
   const int a = 1;
   const int b = 20;
-  std::vector<int> vec(size);
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::generate(vec.begin(), vec.end(), std::bind(std::uniform_int_distribution<>(a, b), gen));
+  std::vector<int> vec = make_rand_data(size, a, b, 1);
   //std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
   //std::cout << std::endl;
   comp_counting_sort(std::begin(vec), std::end(vec));
--- utility/make_rand_data.hpp
+++ utility/make_rand_data.hpp
@@ -7,18 +7,25 @@
 namespace rtcpp {
 
 template <typename T>
-std::vector<T> make_rand_data(std::size_t size, T first, T last)
+std::vector<T> make_rand_data(std::size_t size, T first, T last, int type = 2)
 {
-  // Generates a vector of random numbers in the range [first, last]
-  // The generated vector has no repeated elements.
+  // Generates a uniform distribution of integers in the range [first, last]
+  // with size size.
+  // The following options for the argument type are available.
+  // 1 - The returned vector can have repeated elements.
+  // 2 - All elements apear only once. The vector can have size less the "size".
 
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> dis(first, last);
+
   std::vector<T> data;
   std::generate_n( std::back_inserter(data)
                  , size
                  , [&](){return dis(gen);});
+  if (type == 1)
+    return data;
+
   std::sort(std::begin(data), std::end(data));
   //std::unique(std::begin(data), std::end(data));
   data.erase(std::unique(std::begin(data), std::end(data)), std::end(data));
