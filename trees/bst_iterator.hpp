ca1d1dec68b6b20ee30822119f6cf2c1a0544cb6
Marcelo <mzimbres@gmail.com>
2014-10-10 15:14:28 -0300

    Use of std::iterator in bst.

--- trees/bst_iterator.hpp
+++ trees/bst_iterator.hpp
@@ -1,17 +1,14 @@
 #pragma once
 
+#include <iterator>
+
 #include "bst_node.hpp"
 
 namespace rtcpp {
 
 template <typename T>
-class bst_iterator {
+class bst_iterator : public std::iterator<std::bidirectional_iterator_tag, const T> {
   public:
-  typedef T value_type;
-  typedef const T* pointer;
-  typedef const T& reference;
-  typedef std::bidirectional_iterator_tag iterator_category;
-  typedef std::ptrdiff_t difference_type;
   typedef node<T> node_type;
   typedef const node_type* node_pointer;
   private:
