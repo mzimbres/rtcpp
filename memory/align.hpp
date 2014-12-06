9c8d7dd27b375b9787f204fc19264283c90acc28
Marcelo <mzimbres@gmail.com>
2014-12-06 01:22:09 -0200

    Ensuring pointer alignement on continer construction.

---
+++ memory/align.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace rtcpp {
+
+constexpr bool is_power_of_two(std::size_t a) {return !(a & (a - 1));}
+
+// returns true if the remainder of a divided by b is zero, i.e. a % b. b
+// must be a power of two.
+constexpr bool is_aligned(std::size_t a, std::size_t b) {return !(a & (b - 1));}
+
+constexpr std::size_t next_aligned(std::size_t a, std::size_t b) {return (a & ~(b - 1)) + b;}
+
+}
