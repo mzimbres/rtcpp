3c4ef3d28683d4563f22974400a11c8b0087c161
Marcelo Zimbres <mzimbres@gmail.com>
2014-12-20 14:09:08 -0200

    std::reverse_iterator seems to be broken.

--- tests/test_set_iterator.cpp
+++ tests/test_set_iterator.cpp
@@ -19,26 +19,22 @@ int main()
 
   // Now the tree has three items 3, 2, 4. Lets test if the iterators can get
   // us to the right point.
-  typedef std::reverse_iterator<set<int>::iterator> reverse_iter;
-  reverse_iter iter(t1.end());
+  auto iter = t1.end();
 
   // We should be now at the rbegin.
-  if (*iter != 5)
+  if (*--iter != 5)
     return 1;
 
-  if (*++iter != 4)
+  if (*--iter != 4)
     return 1;
 
-  if (*++iter != 3)
+  if (*--iter != 3)
     return 1;
 
-  if (*++iter != 2)
+  if (*--iter != 2)
     return 1;
 
-  if (*++iter != 1)
-    return 1;
-
-  if (!std::equal(t1.rbegin(), t1.rend(), arr.begin()))
+  if (*--iter != 1)
     return 1;
 
   if (!std::equal(t1.begin(), t1.end(), arr.rbegin()))
