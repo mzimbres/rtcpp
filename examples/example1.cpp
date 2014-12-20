706ee912f0ee270f9c96de11cb719962ca112bc5
Marcelo <mzimbres@gmail.com>
2014-12-20 13:17:57 -0200

    Removal of exemple3. Examples are now using std::set instead of rt::set.

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <iomanip>
 #include <array>
-#include <iterator>
+#include <set>
 
 #include <container/set.hpp>
 #include <memory/allocator.hpp>
 
 int main()
 {
-  rt::set<int> t1 = {5, 3, 7, 20, 1, 44, 22, 8};
+  typedef std::set<int, std::less<int>, rt::allocator<int>> rt_set_type;
+
+  std::array<char, 2000> buffer = {{}};
+  rt::allocator<int> alloc(buffer);
+
+  rt_set_type t1(std::less<int>(), alloc);
+  t1 = {5, 3, 7, 20, 1, 44, 22, 8};
 
   std::copy( std::begin(t1)
            , std::end(t1)
--- examples/example2.cpp
+++ examples/example2.cpp
@@ -2,20 +2,32 @@
 #include <iomanip>
 #include <array>
 #include <iterator>
+#include <set>
 
 #include <container/set.hpp>
 #include <memory/allocator.hpp>
 
 int main()
 {
+  typedef std::set<int, std::less<int>, rt::allocator<int>> rt_set_type;
+
   std::array<char, 2000> buffer = {{}};
   rt::allocator<int> alloc(buffer);
-  rt::set<int> t1(alloc);
 
-  t1 = {5, 3, 7, 20, 1, 44, 22, 8, 44, 33, 9, 2, 99, 77};
+  rt_set_type t1(std::less<int>(), alloc);
+  rt_set_type t2(std::less<int>(), alloc);
+
+  t1 = {5, 3, 7, 20, 1};
+  t2 = {44, 22, 8, 44, 33};
 
   std::copy( std::begin(t1)
            , std::end(t1)
+           , std::ostream_iterator<int>(std::cout, " "));
+
+  std::cout << std::endl;
+
+  std::copy( std::begin(t2)
+           , std::end(t2)
            , std::ostream_iterator<int>(std::cout, " "));
 
   std::cout << std::endl;
