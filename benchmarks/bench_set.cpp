e9540fdbb6ee5d947c0852fb45c7f33ac8c37a85
Marcelo Zimbres <mzimbres@gmail.com>
2014-12-20 14:59:51 -0200

    Improvements in benchmark program.

--- benchmarks/bench_set.cpp
+++ benchmarks/bench_set.cpp
@@ -14,79 +14,61 @@
 #include <utility/make_rand_data.hpp>
 #include <utility/timer.hpp>
 
+template <typename C>
+void print_bench(C& c, const std::vector<int>& vec)
+{
+  {
+    std::cout << "Insertion: : ";
+    rt::timer t;
+    c.insert(std::begin(vec), std::end(vec));
+  }
+  {
+    std::cout << "Lookup:    : ";
+    rt::timer t;
+    c.insert(std::begin(vec), std::end(vec));
+  }
+  {
+    std::cout << "Deletion:  : ";
+    rt::timer t;
+    c.clear();
+  }
+}
+
 int main(int argc, char* argv[])
 {
   std::size_t size = 600000;
   if (argc == 2)
     size = to_number<std::size_t>(argv[1]);
 
-  using namespace rt;
-
   const int a = 1;
   const int b = std::numeric_limits<int>::max();
   //const int b = size;
 
-  std::vector<int> data = make_rand_data(size, a, b);
-  std::vector<int> data_copy = data;
-
-  std::vector<char> buffer((data.size() + 2) * sizeof (set<int>::node_type));
-
-  // The three containers we will benchmark.
-  set<int> t1(allocator<int>(&buffer.front(), buffer.size())); // Uses a vector as buffer.
-  set<int, std::less<int>, std::allocator<int>> t3((std::allocator<int>())); // Uses an allocator (more fragmented)
-  std::set<int> t4;
+  std::vector<int> data = rt::make_rand_data(size, a, b);
 
   std::cout << "N = " << data.size() << std::endl;
   // Benchmarks.
   {
-    std::cout << "Insertion: rt::set<rt::alloc>   : ";
-    timer t;
-    t1.insert(std::begin(data), std::end(data));
+    std::cout << "rt::set<std::alloc>: " << std::endl;
+    rt::set<int, std::less<int>, std::allocator<int>> s((std::allocator<int>()));
+    print_bench(s, data);
   }
   {
-    std::cout << "Insertion: rt::set<std::alloc>  : ";
-    timer t;
-    t3.insert(std::begin(data), std::end(data));
+    std::cout << "rt::set<rt::alloc>: " << std::endl;
+    std::vector<char> buffer((data.size() + 2) * sizeof (rt::set<int>::node_type), 0);
+    rt::set<int> s(rt::allocator<int>(&buffer.front(), buffer.size())); // Uses a vector as buffer.
+    print_bench(s, data);
   }
   {
-    std::cout << "Insertion: std::set<std::alloc> : ";
-    timer t;
-    t4.insert(std::begin(data), std::end(data));
+    std::cout << "std::set<std::alloc>: " << std::endl;
+    std::set<int> s;
+    print_bench(s, data);
   }
   {
-    std::cout << "Sort with std::sort             : ";
-    timer t;
-    std::sort(std::begin(data_copy), std::end(data_copy));
-  }
-  {
-    std::cout << "Lookup: rt::set<rt::alloc>      : ";
-    timer t;
-    t1.insert(std::begin(data), std::end(data));
-  }
-  {
-    std::cout << "Lookup: rt::set<std::alloc>     : ";
-    timer t;
-    t3.insert(std::begin(data), std::end(data));
-  }
-  {
-    std::cout << "Lookup: std::set<std::alloc>    : ";
-    timer t;
-    t4.insert(std::begin(data), std::end(data));
-  }
-  {
-    std::cout << "Deletion: rt::set<rt::alloc>    : ";
-    timer t;
-    t1.clear();
-  }
-  {
-    std::cout << "Deletion: rt::set<std::alloc>   : ";
-    timer t;
-    t3.clear();
-  }
-  {
-    std::cout << "Deletion: std::set<std::alloc>  : ";
-    timer t;
-    t4.clear();
+    std::cout << "std::set<rt::alloc>: " << std::endl;
+    std::vector<char> buffer((data.size() + 2) * (2 * sizeof (rt::set<int>::node_type)), 0);
+    std::set<int, std::less<int>, rt::allocator<int>> s(rt::allocator<int>(&buffer.front(), buffer.size()));
+    print_bench(s, data);
   }
   std::cout << std::endl;
 
