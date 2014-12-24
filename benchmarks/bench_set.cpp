f8fdec049f887182e1513caef911c1e4ba46dfdd
Marcelo Zimbres <mzimbres@gmail.com>
2014-12-24 14:07:49 -0200

    More improvements in benchmark program.

--- benchmarks/bench_set.cpp
+++ benchmarks/bench_set.cpp
@@ -14,51 +14,69 @@
 #include <utility/make_rand_data.hpp>
 #include <utility/timer.hpp>
 
-template <typename C>
-void print_bench(C& c, const std::vector<int>& vec)
+template <typename C, typename Iter>
+void print_bench(C& c, Iter iter, std::size_t n)
 {
   rt::timer t;
-  c.insert(std::begin(vec), std::end(vec));
+  c.insert(iter, iter + n);
 }
 
 int main(int argc, char* argv[])
 {
-  std::size_t size = 600000;
-  if (argc == 2)
-    size = to_number<std::size_t>(argv[1]);
+  if (argc != 4) {
+    std::cout <<
+    "\nUsage: $ ./bench_set N S K\n"
+    "N: The start size.\n"
+    "S: The step size.\n"
+    "K: How many steps.\n"
+    << std::endl;
+    std::cout <<
+    "The benchmarks have the following layout: \n"
+    "N (rt::set<std::alloc>) (rt::set<rt::alloc>) (std::set<std::alloc>) (std::set<rt::alloc>): \n"
+    << std::endl;
+
+    return 0;
+  }
+
+  const std::size_t N = to_number<std::size_t>(argv[1]);
+  const std::size_t S = to_number<std::size_t>(argv[2]);
+  const std::size_t K = to_number<std::size_t>(argv[3]);
+
+  if (S > N) {
+    std::cout << "S must be less than N." << std::endl;
+    return 1;
+  }
 
   const int a = 1;
   const int b = std::numeric_limits<int>::max();
-  //const int b = size;
 
-  std::cout << std::endl;
-  std::cout <<
-  "The benchmarks have the following layout: \n"
-  "N (rt::set<std::alloc>) (rt::set<rt::alloc>) (std::set<std::alloc>) (std::set<rt::alloc>): \n"
-  << std::endl;
-
+  const std::size_t size = N + K * S;
   std::vector<int> data = rt::make_rand_data(size, a, b);
 
-  std::cout << data.size() << " ";
-  {
-    rt::set<int> s;
-    print_bench(s, data);
-  }
-  {
-    std::vector<char> buffer((data.size() + 2) * sizeof (rt::set<int>::node_type), 0);
-    rt::allocator<int> alloc(buffer);
-    rt::set<int, std::less<int>, rt::allocator<int>> s(alloc); // Uses a vector as buffer.
-    print_bench(s, data);
-  }
-  {
-    std::set<int> s;
-    print_bench(s, data);
-  }
-  {
-    std::vector<char> buffer((data.size() + 2) * (2 * sizeof (rt::set<int>::node_type)), 0);
-    rt::allocator<int> alloc(buffer);
-    std::set<int, std::less<int>, rt::allocator<int>> s(std::less<int>(), alloc);
-    print_bench(s, data);
+  for (std::size_t i = 0; i < K; ++i) {
+    const std::size_t ss = N + i * S;
+    std::cout << ss << " ";
+    {
+      rt::set<int> s;
+      print_bench(s, std::begin(data), ss);
+    }
+    {
+      std::vector<char> buffer((ss + 2) * sizeof (rt::set<int>::node_type), 0);
+      rt::allocator<int> alloc(buffer);
+      rt::set<int, std::less<int>, rt::allocator<int>> s(alloc); // Uses a vector as buffer.
+      print_bench(s, std::begin(data), ss);
+    }
+    {
+      std::set<int> s;
+      print_bench(s, std::begin(data), ss);
+    }
+    {
+      std::vector<char> buffer((ss + 2) * (2 * sizeof (rt::set<int>::node_type)), 0);
+      rt::allocator<int> alloc(buffer);
+      std::set<int, std::less<int>, rt::allocator<int>> s(std::less<int>(), alloc);
+      print_bench(s, std::begin(data), ss);
+    }
+    std::cout << std::endl;
   }
   std::cout << std::endl;
 
