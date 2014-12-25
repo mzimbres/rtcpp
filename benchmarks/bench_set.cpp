d64e7370ff1158c5676f38075ecd5c1e36208333
Marcelo <mzimbres@gmail.com>
2014-12-25 13:21:19 -0200

    Addition of an option to benchmark on a ragmented heap.

--- benchmarks/bench_set.cpp
+++ benchmarks/bench_set.cpp
@@ -15,20 +15,29 @@
 #include <utility/timer.hpp>
 
 template <typename C, typename Iter>
-void print_bench(C& c, Iter iter, std::size_t n)
+void print_bench(C& c, Iter begin, std::size_t n, bool frag)
 {
+  rt::set<int> s1;
+  if (frag) { // Lets us fragment the heap.
+    std::set<int> s2;
+    for (Iter iter = begin; iter != begin + n; ++iter) {
+      s1.insert(*iter);
+      s2.insert(*iter);
+    }
+  } // s2 is destructed leaving many holes in the heap.
   rt::timer t;
-  c.insert(iter, iter + n);
+  c.insert(begin, begin + n);
 }
 
 int main(int argc, char* argv[])
 {
-  if (argc != 4) {
+  if ((argc != 4) && (argc != 5)) {
     std::cout <<
-    "\nUsage: $ ./bench_set N S K\n"
+    "\nUsage: $ ./bench_set N S K F\n"
     "N: The start size.\n"
     "S: The step size.\n"
     "K: How many steps.\n"
+    "F: Optional. If provided will not fragment the heap before benchmarks.\n"
     << std::endl;
     std::cout <<
     "The benchmarks have the following layout: \n"
@@ -41,6 +50,9 @@ int main(int argc, char* argv[])
   const std::size_t N = to_number<std::size_t>(argv[1]);
   const std::size_t S = to_number<std::size_t>(argv[2]);
   const std::size_t K = to_number<std::size_t>(argv[3]);
+  bool frag = true;
+  if (argc == 5)
+    frag = false;
 
   if (S > N) {
     std::cout << "S must be less than N." << std::endl;
@@ -58,23 +70,23 @@ int main(int argc, char* argv[])
     std::cout << ss << " ";
     {
       rt::set<int> s;
-      print_bench(s, std::begin(data), ss);
+      print_bench(s, std::begin(data), ss, frag);
     }
     {
       std::vector<char> buffer((ss + 2) * sizeof (rt::set<int>::node_type), 0);
       rt::allocator<int> alloc(buffer);
       rt::set<int, std::less<int>, rt::allocator<int>> s(alloc); // Uses a vector as buffer.
-      print_bench(s, std::begin(data), ss);
+      print_bench(s, std::begin(data), ss, frag);
     }
     {
       std::set<int> s;
-      print_bench(s, std::begin(data), ss);
+      print_bench(s, std::begin(data), ss, frag);
     }
     {
       std::vector<char> buffer((ss + 2) * (2 * sizeof (rt::set<int>::node_type)), 0);
       rt::allocator<int> alloc(buffer);
       std::set<int, std::less<int>, rt::allocator<int>> s(std::less<int>(), alloc);
-      print_bench(s, std::begin(data), ss);
+      print_bench(s, std::begin(data), ss, frag);
     }
     std::cout << std::endl;
   }
