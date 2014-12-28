141f85c4a04441733bca5ddc22188f4885713970
Marcelo <mzimbres@gmail.com>
2014-12-28 19:35:11 -0200

    Improvements in bench_map program.

--- benchmarks/bench_map.cpp
+++ benchmarks/bench_map.cpp
@@ -5,26 +5,40 @@
 #include <functional>
 #include <algorithm>
 #include <map>
+#include <set>
 #include <utility>
 
 #include <memory/allocator.hpp>
+#include <container/set.hpp>
 #include <utility/to_number.hpp>
 #include <utility/make_rand_data.hpp>
 #include <utility/timer.hpp>
 
 template <typename Iter>
-void heap_frag(std::map<int, int>& s1, std::map<int, int>& s3, Iter begin, std::size_t n)
+void heap_frag( rt::set<int>& s1
+              , rt::set<int>& s2
+              , rt::set<int>& s3
+              , std::map<int, int>& s4
+              , Iter begin
+              , std::size_t n)
 {
   s1.clear();
+  s2.clear();
   s3.clear();
-  std::map<int, int> s2;
-  std::map<int, int> s4;
+  s4.clear();
+  rt::set<int> s5;
+  rt::set<int> s6;
+  rt::set<int> s7;
+  std::map<int, int> s8;
   for (Iter iter = begin; iter != begin + n; ++iter) {
-    auto val = *iter;
-    s1.insert(std::make_pair(val, val));
-    s2.insert(std::make_pair(val, val));
-    s3.insert(std::make_pair(val, val));
-    s4.insert(std::make_pair(val, val));
+    s1.insert(*iter);
+    s5.insert(*iter);
+    s2.insert(*iter);
+    s6.insert(*iter);
+    s3.insert(*iter);
+    s7.insert(*iter);
+    s4.insert(std::make_pair(*iter, *iter));
+    s8.insert(std::make_pair(*iter, *iter));
   }
   // s2 and s4 are destructed leaving many holes in the heap.
 }
@@ -75,10 +89,12 @@ int main(int argc, char* argv[])
   const std::size_t size = N + K * S;
   std::vector<int> data = rt::make_rand_data<int>(size, a, b);
 
-  std::map<int, int> s1;
-  std::map<int, int> s2;
+  rt::set<int> s1;
+  rt::set<int> s2;
+  rt::set<int> s3;
+  std::map<int, int> s4;
   if (frag) 
-    heap_frag(s1, s2, std::begin(data), data.size());
+    heap_frag(s1, s2, s3, s4, std::begin(data), data.size());
 
   for (std::size_t i = 0; i < K; ++i) {
     const std::size_t ss = N + i * S;
