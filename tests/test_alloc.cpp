8c6a85d63078ae449b97c3d331f97a5f2dc48530
Marcelo <mzimbres@gmail.com>
2014-12-29 15:55:26 -0200

    Test for rt::allocator with std::list.

--- tests/test_alloc.cpp
+++ tests/test_alloc.cpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <set>
 #include <algorithm>
+#include <list>
 
 #include <memory/allocator.hpp>
 #include <container/set.hpp>
@@ -35,14 +36,45 @@ bool test_link_diff_node_size()
   return false;
 }
 
+bool test_list()
+{
+  std::array<int, 6> data = {{10, 3, 2, 8, 19, 33}};
+
+  std::array<char, 5000> buffer = {{}};
+
+  rt::allocator<int> alloc(buffer);
+
+  std::list<int, rt::allocator<int>> t1(std::begin(data), std::end(data), alloc);
+  std::list<int, rt::allocator<int>> t2(std::begin(data), std::end(data), alloc);
+  std::list<int, rt::allocator<int>> t3(std::begin(data), std::end(data), alloc);
+
+  if (!std::equal(std::begin(data), std::end(data), std::begin(t1)))
+    return false;
+
+  if (!std::equal(std::begin(data), std::end(data), std::begin(t2)))
+    return false;
+
+  if (!std::equal(std::begin(data), std::end(data), std::begin(t3)))
+    return false;
+
+  return true;
+}
+
 int main()
 {
-  if (!test_buffer_size())
-    return 1;
+  try {
+    if (!test_buffer_size())
+      return 1;
 
-  if (!test_link_diff_node_size())
-    return 1;
+    if (!test_link_diff_node_size())
+      return 1;
 
+    if (!test_list())
+      return 1;
+  } catch (const std::exception& e) {
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
   return 0;
 }
 
